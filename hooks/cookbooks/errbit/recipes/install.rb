17b6d7d12beb3fa6673997d494c9a1a238df97b5
Charles Butler <chuck@dasroot.net>
2013-12-15 05:38:52 -0500

    importing more scaffolding for install script

--- hooks/cookbooks/errbit/recipes/install.rb
+++ hooks/cookbooks/errbit/recipes/install.rb
@@ -1,28 +1,83 @@
 
-#declare configurable settings
-repository = config_get['repository']
-release = config_get['release']
+#include the apt cookbook for the LWRPS
+include_recipe "apt"
 
-errbit_id = `id errbit`
-
-juju_log("Creating Errbit user if not found")
-execute "adduser --disabled-password --gecos 'Errbit' errbit" do
-	not_if errbit_id != ''
+apt_repository "mongodb-10gen" do
+  uri "http://downloads-distro.mongodb.org/repo/ubuntu-upstart"
+  distribution "dist"
+  components ["10gen"]
+  keyserver "keyserver.ubuntu.com"
+  key "7F0CEB10"
 end
+
+#TODO: check if db relation exists and short ciruit
+package "mongodb-10gen" do
+  action :install
+end
+
+#install dependencies
+package "libxslt-dev" do
+  action :install
+end
+
+package "libxml2-dev" do
+  action :install
+end
+
+
+user "errbit" do
+  shell "/bin/bash"
+  home "/home/errbit"
+  system true
+  supports :manage_home => true
+  action :create
+  uid 3000
+end
+
 
 git "/home/errbit/errbit" do
-	repository config_get['repository']
-	reference config_get['release']
-	action :sync
+  repository config_get['repository']
+  reference config_get['release']
+  action :sync
 end
+
+#drop the templates
+template "/home/errbit/errbit/config/config.yml" do
+  owner "errbit"
+  group "errbit"
+  mode "0660"
+  source "config.yml.erb"
+  variables({
+   confirm_resolve: config_get['confirm_resolve'],
+   gravatar: config_get['gravatar'],
+   smtp_host: config_get['smtp_host'],
+   smtp_domain: config_get['smtp_domain'],
+   smtp_port: config_get['smtp_port'],
+   smtp_starttls_auto: config_get['smtp_starttls_auto'],
+   smtp_user: config_get['smtp_user'],
+   smtp_pass: config_get['smtp_pass']
+   })
+end
+
+#Prep the hack script to circumvent chef's personality conflicts
+template "/tmp/bundler.sh" do
+  action :create
+  owner 'errbit'
+  group 'errbit'
+  mode 0777
+  source 'bundler.sh.erb'
+end
+
+
 
 directory "/home/errbit/errbit" do
-	owner "errbit"
-	group "errbit"
-	recursive true
+  owner "errbit"
+  group "errbit"
+  recursive true
 end
 
-execute "bundle install --no-ri --no-rdoc --without test --deployment" do
-	cwd "/home/errbit/errbit"
-	not_if 'bundle check'
+execute "chown -R errbit:errbit /home/errbit/errbit" do
+  action :run
 end
+
+
