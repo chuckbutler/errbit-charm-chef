cf8d6a334a4f03c96ef2276fe6433205a226de3d
Charles Butler <chuck@dasroot.net>
2013-12-16 00:17:03 -0500

    Working installer on vanilla Ubuntu 12.04 - version tender lovins

---
+++ hooks/cookbooks/errbit/recipes/config-changed.rb
@@ -0,0 +1,98 @@
+
+#drop the templates
+template "/home/errbit/errbit/config/config.yml" do
+  owner "errbit"
+  group "errbit"
+  mode "0660"
+  source "config.yml.erb"
+  variables({
+   confirm_resolve: config_get['confirm_resolve'],
+   gravatar: config_get['gravatar'],
+   smtp_host: config_get['smtp_host'],
+   smtp_domain: config_get['smtp_domain'],
+   smtp_port: config_get['smtp_port'],
+   smtp_starttls_auto: config_get['smtp_starttls_auto'],
+   smtp_user: config_get['smtp_user'],
+   smtp_pass: config_get['smtp_pass']
+   })
+end
+
+template "/home/errbit/errbit/config/mongoid.yml" do
+  owner "errbit"
+  group "errbit"
+  mode "0660"
+  source "mongoid.yml.erb"
+end
+
+#delete existing upstart templates if they exist
+template "/etc/init/errbit.conf" do
+  action :delete
+end
+
+template "/etc/init/errbit-web.conf" do
+  action :delete
+end
+
+#setup upstart templates
+template "/etc/init/errbit.conf" do
+  action :create
+  owner 'root'
+  group 'root'
+  mode '0644'
+  source 'errbit.conf.erb'
+end
+
+template "/etc/init/errbit-web.conf" do
+  action :create
+  owner 'root'
+  group 'root'
+  mode '0644'
+  source 'errbit-web.conf.erb'
+end
+
+
+template "/home/errbit/errbit/config/unicorn.rb" do
+  action :create
+  owner 'errbit'
+  group 'errbit'
+  mode 0644
+  source 'unicorn.rb.erb'
+  variables({
+    unicorn_workers: config_get['unicorn_workers']
+  })
+end
+
+
+#setup the NGINX configuration
+template "/etc/nginx/sites-available/errbit" do
+  action :create
+  owner 'root'
+  group 'root'
+  mode 0644
+  source 'errbit-nginx.erb'
+  variables({
+    hostname: config_get['hostname']
+  })
+end
+
+link "/etc/nginx/sites-enabled/errbit" do
+  action :create
+  to "/etc/nginx/sites-available/errbit"
+end
+
+#TODO: document that we remove the default nginx vhost
+link "/etc/nginx/sites-enabled/default" do
+  action :delete
+end
+
+
+#Prep the post-deployment script to circumvent chef's personality conflicts
+template "/tmp/bundler.sh" do
+  action :create
+  owner 'errbit'
+  group 'errbit'
+  mode 0777
+  source 'bundler.sh.erb'
+end
+
+juju_log("Completed template generation")
--- hooks/cookbooks/errbit/recipes/install.rb
+++ hooks/cookbooks/errbit/recipes/install.rb
@@ -24,6 +24,10 @@ package "libxml2-dev" do
   action :install
 end
 
+package "nginx" do
+  action :install
+end
+
 user "errbit" do
   shell "/bin/bash"
   home "/home/errbit"
@@ -38,74 +42,15 @@ git "/home/errbit/errbit" do
   reference config_get['release']
   action :sync
 end
-
-#drop the templates
-template "/home/errbit/errbit/config/config.yml" do
-  owner "errbit"
-  group "errbit"
-  mode "0660"
-  source "config.yml.erb"
-  variables({
-   confirm_resolve: config_get['confirm_resolve'],
-   gravatar: config_get['gravatar'],
-   smtp_host: config_get['smtp_host'],
-   smtp_domain: config_get['smtp_domain'],
-   smtp_port: config_get['smtp_port'],
-   smtp_starttls_auto: config_get['smtp_starttls_auto'],
-   smtp_user: config_get['smtp_user'],
-   smtp_pass: config_get['smtp_pass']
-   })
+juju_port 80 do
+  action :open
 end
 
-template "/home/errbit/errbit/config/mongoid.yml" do
-  owner "errbit"
-  group "errbit"
-  mode "0660"
-  source "mongoid.yml.erb"
-end
-
-#delete existing upstart templates if they exist
-template "/etc/init/errbit.conf" do
-  action :delete
-end
-
-template "/etc/init/errbit-web.conf" do
-  action :delete
-end
-
-#setup upstart template
-template "/etc/init/errbit.conf" do
-  action :create
-  owner 'root'
-  group 'root'
-  mode '0644'
-  source 'errbit.conf.erb'
-end
-
-template "/etc/init/errbit-web.conf" do
-  action :create
-  owner 'root'
-  group 'root'
-  mode '0644'
-  source 'errbit-web.conf.erb'
-end
-
-
-
-#Prep the post-deployment script to circumvent chef's personality conflicts
-template "/tmp/bundler.sh" do
-  action :create
-  owner 'errbit'
-  group 'errbit'
-  mode 0777
-  source 'bundler.sh.erb'
-end
-
-directory "/home/errbit/errbit" do
-  owner "errbit"
-  group "errbit"
-  recursive true
-end
+#directory "/home/errbit/errbit" do
+#  owner "errbit"
+#  group "errbit"
+#  recursive true
+#end
 
 execute "chown -R errbit:errbit /home/errbit/errbit" do
   action :run
