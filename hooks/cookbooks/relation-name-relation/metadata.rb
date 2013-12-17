0c4f9d5386280f502e7d3e1b2deceeaa06443e20
Charles Butler <chuck@dasroot.net>
2013-12-16 22:13:44 -0500

    Adds boilerplate cookbook to gitignore, and untracks the path

--- hooks/cookbooks/relation-name-relation/metadata.rb
+++
@@ -1,8 +0,0 @@
-maintainer       "Altoros Systems, Inc."
-maintainer_email "pavel.pachkovskij@altoros.com"
-license          "GPL-3"
-description      "JuJu Helpers"
-
-version          "0.1"
-name             "relation-name-relation"
-depends          "juju-helpers"
