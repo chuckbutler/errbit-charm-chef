aafb034472f473d7a0763746a7bf78e727e47ec2
Charles Butler <chuck@dasroot.net>
2013-12-18 23:24:15 -0500

    Write the relationship sentinel, and do data migration to remote database connection

--- hooks/cookbooks/mongodb-relation/recipes/changed.rb
+++ hooks/cookbooks/mongodb-relation/recipes/changed.rb
@@ -1,24 +1,33 @@
 require 'securerandom'
 
 mongodb = {
-	host: juju_relation['hostname'],
-	port: juju_relation['port'],
+	host: relation_get['hostname'],
+	port: relation_get['port'],
 	database: 'errbit' 
 	}
 
+# This failout condition will do nothing
+# if we cannot find the relationship details.
 
-if %i(host port).any? { |attr| mongodb[attr].nil? || mongodb[attr].empty? }
-  puts "Waiting for all attributes to be set"
+if [:host, :port].any? { |attr| mongodb[attr].nil? || mongodb[attr].empty? }
+  juju-log("Waiting for all attributes to be set")
 else
  
-  template "/home/errbit/errbit/config/database.yml" do
+  template "/home/errbit/errbit/config/mongoid.yml" do
     variables({
       mongo_uri: "mongodb://#{mongodb[:host]}:#{mongodb[:port]}/#{mongodb[:database]}",
     })
     user 'errbit'
     group 'errbit'
     mode '0644'
+    source "mongoid.yml.erb"
+    cookbook "errbit"
     action :create
+  end
+
+  #deploy the backup data
+  execute "mongorestore" do
+    command "mongorestore -h #{relation_get['hostname']} -d errbit $CHARM_DIR/mongo-data/errbit/"
   end
 
   service 'errbit' do
@@ -26,4 +35,13 @@ else
     provider Chef::Provider::Service::Upstart
     action :restart
   end
+
+  package "mongodb-10gen" do
+    action :purge
+  end
+ 
+  execute "touch $CHARM_DIR/.mongodb" do
+    action :nothing
+  end
+
 end
