c3bcb14584383d22e0d63acb5e5908daba7413a0
Charles Butler <chuck@dasroot.net>
2013-12-22 11:38:07 -0500

    Symlinked the start script to the chef recipe, added chef service handlers to cookbook

--- hooks/cookbooks/errbit/recipes/start.rb
+++ hooks/cookbooks/errbit/recipes/start.rb
@@ -1,3 +1,19 @@
-service 'errbit' do
-	action :start
+service 'nginx' do
+  ignore_failure true
+  provider Chef::Provider::Service::Upstart
+  action :start
 end
+
+service 'nginx' do
+  action :reload
+  ignore_failure true
+  provider Chef::Provider::Service::Upstart
+end
+
+service 'errbit' do
+  ignore_failure true
+  provider Chef::Provider::Service::Upstart
+  action :start
+end
+
+
