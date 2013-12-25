b3210a9c18680af51b990395fa8e24845fbeb8ba
Charles Butler <chuck@dasroot.net>
2013-12-25 11:00:48 -0500

    Removed nginx service restart line, and port modifications from Start hook, made errbit starting without error mandatory - should fail the node if the servie does not come up.

--- hooks/cookbooks/errbit/recipes/start.rb
+++ hooks/cookbooks/errbit/recipes/start.rb
@@ -1,16 +1,6 @@
 
-service 'nginx' do
-  action :restart
-  ignore_failure true
-  provider Chef::Provider::Service::Upstart
-end
-
 service 'errbit' do
-  ignore_failure true
   provider Chef::Provider::Service::Upstart
   action :start
 end
 
-juju_Port "port[80]" do
-  action :open
-end
