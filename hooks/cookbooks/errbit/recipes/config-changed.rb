20b70f989e74ea5a417cd7d5f6b6ec74cee9b2b8
Charles Butler <chuck@dasroot.net>
2013-12-25 13:31:09 -0500

    Cleaned up and reorganized config-changed by moving upstart templates to end of runlist, and grouped application logic at the beginning of the run

--- hooks/cookbooks/errbit/recipes/config-changed.rb
+++ hooks/cookbooks/errbit/recipes/config-changed.rb
@@ -18,7 +18,8 @@ template node[:errbit][:path] + "/config/config.yml" do
    smtp_port: config_get['smtp_port'],
    smtp_starttls_auto: config_get['smtp_starttls_auto'],
    smtp_user: config_get['smtp_user'],
-   smtp_pass: config_get['smtp_pass']
+   smtp_pass: config_get['smtp_pass'],
+   sendmail: config_get['use_sendmail']
    })
 end
 
@@ -28,17 +29,54 @@ end
     mode "0660"
     source "mongoid.yml.erb"
     variables({ mongo_uri: "mongodb://localhost:27017/errbit" })
-    not_if { File.exists?("#{ENV['CHARM_DIR']}/.mongodb") }
+    not_if { File.exists?("#{ENV['CHARM_DIR']}/mongodb") }
   end
 
-#delete existing upstart templates if they exist
-template "/etc/init/errbit.conf" do
-  action :delete
+bash "bundle_deploy" do
+  user "errbit"
+  cwd node[:errbit][:path]
+  code "/usr/bin/env bundle install --without test --deployment"
 end
 
-template "/etc/init/errbit-web.conf" do
-  action :delete
+execute "generate_secret_token" do
+  user "root"
+  cwd node[:errbit][:path]
+  command "echo `sudo -u errbit -H bundle exec rake secret` > $CHARM_DIR/secret_token"
+  action :run
+  not_if { File.exists?("#{ENV['CHARM_DIR']}/secret_token")}
 end
+
+
+template node[:errbit][:path] + "/config/initializers/secret_token.rb" do
+  action :create
+  source 'secret_token.rb.erb'
+  owner "errbit"
+  group "errbit"
+  variables({
+    secret: `cat $CHARM_DIR/secret_token`
+    })
+end
+
+
+execute "bootstrap_errbit" do
+  user "errbit"
+  cwd node[:errbit][:path]
+  action :run
+  command "bundle exec rake errbit:bootstrap && touch #{node[:errbit][:path]}/.strapped"
+  environment ({'RAILS_ENV' => 'production'})
+  creates node[:errbit][:path] + "/.strapped"
+end
+
+
+
+recompile_assets()
+
+execute "cache_git_revision" do
+    command "cd #{node[:errbit][:path]} && git rev-parse HEAD > #{ENV['CHARM_DIR']}/errbit_revision"
+    action :run
+end
+
+
 
 #setup upstart templates
 template "/etc/init/errbit.conf" do
@@ -70,89 +108,9 @@ template node[:errbit][:path] + "/config/unicorn.rb" do
 end
 
 
-#setup the NGINX configuration
-template "/etc/nginx/sites-available/errbit" do
-  action :create
-  owner 'root'
-  group 'root'
-  mode 0644
-  source 'errbit-nginx.erb'
-  variables({
-    hostname: config_get['hostname']
-  })
-end
-
-execute "service nginx reload" do
-  action :run
-end
-
-link "/etc/nginx/sites-enabled/errbit" do
-  action :create
-  to "/etc/nginx/sites-available/errbit"
-end
-
-#TODO: document that we remove the default nginx vhost
-link "/etc/nginx/sites-enabled/default" do
-  action :delete
-end
-
-#this is a work around due to bundlers sandboxing
-template "/tmp/bundle_install.sh" do
-  action :create
-  owner 'errbit'
-  group 'errbit'
-  mode '0755'
-  source 'bundler.sh.erb'
-  variables({
-    path: node[:errbit][:path]
-    })
-end
-
-execute "/tmp/bundle_install.sh" do
-  user "errbit"
-  cwd node[:errbit][:path]
-  command "/tmp/bundle_install.sh"
-  action :run
-end
-
-bash "bundle_deploy" do
-  user "errbit"
-  cwd node[:errbit][:path]
-  code "/usr/bin/env bundle install --without test --deployment"
-end
-
-execute "generate_secret_token" do
-  user "root"
-  cwd node[:errbit][:path]
-  command "echo `sudo -u errbit -H bundle exec rake secret` > $CHARM_DIR/secret_token"
-  action :run
-  not_if { File.exists?("#{ENV['CHARM_DIR']}/secret_token")}
-end
-
-template node[:errbit][:path] + "/config/initializers/secret_token.rb" do
-  action :create
-  source 'secret_token.rb.erb'
-  owner "errbit"
-  group "errbit"
-  variables({
-    secret: `cat $CHARM_DIR/secret_token`
-    })
-end
-
-execute "bootstrap_errbit" do
-  user "errbit"
-  cwd node[:errbit][:path]
-  action :run
-  command "bundle exec rake errbit:bootstrap && touch #{node[:errbit][:path]}/.strapped"
-  environment ({'RAILS_ENV' => 'production'})
-  creates node[:errbit][:path] + "/.strapped"
-end
 
 
 
-recompile_assets()
 
-execute "cache_git_revision" do
-    command "cd #{node[:errbit][:path]} && git rev-parse HEAD > #{ENV['CHARM_DIR']}/errbit_revision"
-    action :run
-end
+
+
